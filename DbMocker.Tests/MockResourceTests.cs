38be351bf63d818245a1e703a93be82a407ef043
Denis Voituron <denis@voituron.net>
2020-11-06 22:09:59 +0100

    Add MockResource to split content

---
+++ DbMocker.Tests/MockResourceTests.cs
@@ -0,0 +1,112 @@
+﻿using Apps72.Dev.Data.DbMocker;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbMocker.Tests
+{
+    [TestClass]
+    public class MockResourceTests
+    {
+        [TestMethod]
+        public void MockResource_SplitContent_Returns_Test()
+        {
+            string content = "ab\ncd\ref\n\rgh\r\nij";
+            var resource = new MockResource(content);
+
+            Assert.AreEqual(5, resource.Lines.Count());
+            Assert.AreEqual("ab", resource.Lines.ElementAt(0));
+            Assert.AreEqual("cd", resource.Lines.ElementAt(1));
+            Assert.AreEqual("ef", resource.Lines.ElementAt(2));
+            Assert.AreEqual("gh", resource.Lines.ElementAt(3));
+            Assert.AreEqual("ij", resource.Lines.ElementAt(4));
+        }
+
+        [TestMethod]
+        public void MockResource_SplitContent_Comments_Test()
+        {
+            string content = "ab" + Environment.NewLine +
+                             "#cd" + Environment.NewLine +
+                             "ef" + Environment.NewLine;
+            var resource = new MockResource(content);
+
+            Assert.AreEqual(2, resource.Lines.Count());
+            Assert.AreEqual("ab", resource.Lines.ElementAt(0));
+            Assert.AreEqual("ef", resource.Lines.ElementAt(1));
+        }
+
+        [TestMethod]
+        public void MockResource_ColumnPositions_Test()
+        {
+            string content = "Col1    Col2   Col3  \n" +
+                             "Data1   Data2  Data3   ";
+
+            var resource = new MockResource(content);
+            var fields = resource.Fields.ToArray();
+
+            Assert.AreEqual(3, fields.Length);
+            Assert.AreEqual("Col1", fields[0].Name);
+            Assert.AreEqual("Col2", fields[1].Name);
+            Assert.AreEqual("Col3", fields[2].Name);
+            Assert.AreEqual("Data1", fields[0].DataType);
+            Assert.AreEqual("Data2", fields[1].DataType);
+            Assert.AreEqual("Data3", fields[2].DataType);
+            Assert.AreEqual(0, fields[0].Position);
+            Assert.AreEqual(8, fields[1].Position);
+            Assert.AreEqual(15, fields[2].Position);
+        }
+
+        [TestMethod]
+        public void MockResource_MoreColumns_Test()
+        {
+            string content = "Col1    Col2 \n" +
+                             "Data1";
+
+            var resource = new MockResource(content);
+            var fields = resource.Fields.ToArray();
+
+            Assert.AreEqual(2, fields.Length);
+            Assert.AreEqual("Col1", fields[0].Name);
+            Assert.AreEqual("Col2", fields[1].Name);
+            Assert.AreEqual("Data1", fields[0].DataType);
+            Assert.AreEqual(null, fields[1].DataType);
+            Assert.AreEqual(0, fields[0].Position);
+            Assert.AreEqual(8, fields[1].Position);
+        }
+
+        [TestMethod]
+        public void MockResource_MoreDataTypes_Test()
+        {
+            string content = "Col1           \n" +
+                             "Data1    Data2   ";
+
+            var resource = new MockResource(content);
+            var fields = resource.Fields.ToArray();
+
+            Assert.AreEqual(1, fields.Length);
+            Assert.AreEqual("Col1", fields[0].Name);
+            Assert.AreEqual("Data1    Data2", fields[0].DataType);
+            Assert.AreEqual(0, fields[0].Position);
+        }
+
+        [TestMethod]
+        public void MockResource_Rows_Test()
+        {
+            string content = "Col1    Col2    \n" +
+                             "Type1   Type2   \n" +
+                             "Value1  Value2  \n" +
+                             "Value3  Value4  \n";
+
+            var resource = new MockResource(content);
+            var rows = resource.Rows.ToArray();
+
+            Assert.AreEqual("Value1", rows[0][0]);
+            Assert.AreEqual("Value2", rows[0][1]);
+            Assert.AreEqual("Value3", rows[1][0]);
+            Assert.AreEqual("Value4", rows[1][1]);
+        }
+    }
+}
---
+++ DbMocker/MockResource.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Apps72.Dev.Data.DbMocker
+{
+    /// <summary />
+    public class MockResource
+    {
+        /// <summary>
+        /// Convert the fixed fields string to a MockResource, with a MockTable.
+        /// </summary>
+        /// <param name="content"></param>
+        public MockResource(string content)
+        {
+            // All lines (without commented rows)
+            this.Lines = content.Split(new char[] { '\n', '\r' })
+                                .Where(line => String.IsNullOrWhiteSpace(line) == false)
+                                .Where(line => line.StartsWith("#") == false);
+
+            // Fields positions
+            string columnsRow = this.Lines?.ElementAtOrDefault(0);
+            var positions = GetPositions(columnsRow).ToArray();
+
+            // Fields name
+            var fieldsNames = SplitAtPositions(columnsRow, positions);
+
+            // Data types
+            string dataTypesRow = this.Lines?.ElementAtOrDefault(1);
+            var dataTypes = SplitAtPositions(dataTypesRow, positions);
+
+            // Combine to Fields
+            this.Fields = positions.Select(i => new Field()
+            { 
+                Position = i,
+                Name = fieldsNames.ContainsKey(i) ? fieldsNames[i] : null,
+                DataType = dataTypes.ContainsKey(i) ? dataTypes[i] : null
+            });
+
+            // Rows
+            var rows = new List<string[]>();
+            for (int i = 2; i < this.Lines.Count(); i++)
+            {
+                var newRow = SplitAtPositions(this.Lines.ElementAt(i), positions).Select(item => item.Value);
+                rows.Add(newRow.ToArray());
+            }
+            this.Rows = rows;
+        }
+
+        /// <summary>
+        /// Convert the fixed fields text embedded file to a MockResource, with a MockTable.
+        /// </summary>
+        /// <param name="assembly"></param>
+        /// <param name="resourceName"></param>
+        public MockResource(Assembly assembly, string resourceName)
+            : this(ReadResourceFile(assembly, resourceName))
+        {
+            
+        }
+
+        /// <summary>
+        /// Gets all lines included in the resource file.
+        /// </summary>
+        public IEnumerable<string> Lines { get; }
+
+        /// <summary>
+        /// Gets all lines splitted, included in the resource file.
+        /// </summary>
+        public IEnumerable<string[]> Rows { get; }
+
+        /// <summary>
+        /// Gets fields description
+        /// </summary>
+        public IEnumerable<Field> Fields { get; }
+
+        /// <summary>
+        /// Split specified text line to data using fixed positions.
+        /// </summary>
+        /// <param name="line"></param>
+        /// <param name="positions"></param>
+        /// <returns></returns>
+        private Dictionary<int, string> SplitAtPositions(string line, int[] positions)
+        {
+            var result = new Dictionary<int, string>();
+            int numberOfPosition = positions.Length;
+
+            if (!String.IsNullOrEmpty(line))
+            {
+                for (int i = 0; i < numberOfPosition; i++)
+                {
+                    int indexFrom = positions[i];
+                    int? indexTo = (i + 1) < numberOfPosition
+                                 ? positions[i + 1]
+                                 : (int?)null;
+
+                    if (indexFrom < line.Length)
+                    {
+                        string part = indexTo != null && indexTo < line.Length
+                                    ? line.Substring(indexFrom, indexTo.Value - indexFrom)
+                                    : line.Substring(indexFrom);
+
+                        part = part.Trim();
+
+                        result.Add(indexFrom, part);
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Gets all index positions of words
+        /// "ABC 123" => [0, 4]
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        private IEnumerable<int> GetPositions(string line)
+        {
+            var positions = new List<int>();
+            bool findNext = true;
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                char item = line[i];
+
+                if (item == ' ' || item == '\t')
+                    findNext = true;
+
+                if (findNext && item != ' ' && item != '\t')
+                {
+                    positions.Add(i);
+                    findNext = false;
+                }
+            }
+
+            return positions;
+        }
+
+        /// <summary>
+        /// Read the resource text file.
+        /// </summary>
+        /// <param name="assembly"></param>
+        /// <param name="resourceName"></param>
+        /// <returns></returns>
+        private static string ReadResourceFile(Assembly assembly, string resourceName)
+        {
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();                
+            }
+        }
+
+        /// <summary />
+        [DebuggerDisplay("[{Position}] {Name} ({DataType})")]
+        public class Field
+        {
+            /// <summary />
+            public string Name { get; internal set; }
+            /// <summary />
+            public int Position { get; internal set; }
+            /// <summary />
+            public string DataType { get; internal set; }
+        }
+    }
+}
